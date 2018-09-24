e23c583c5649927449e127b1425236cad6ed3330
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-24 13:55:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
 class Student
-  
+  def hello  
+    it "returns a greeting phrase" do 
+    expect{student.hello}.to output("Hey there! I'm so excited to learn stuff.\n").to_stdout
+    end
+  end
+
+  describe "#raise_hand" do 
+    it "returns the phrase 'Pick me!'" do 
+    expect{student.raise_hand}.to output("Pick me!\n").to_stdout
+    end
+  end
 end
